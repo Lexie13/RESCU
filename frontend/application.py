4d9748e9aa1b0cef49262b3ddde63c9c304e4385
Bakreed <baker785@purdue.edu>
2026-04-10 02:51:32 -0400

    Update application.py

--- frontend/application.py
+++ frontend/application.py
@@ -3,9 +3,7 @@ from authlib.integrations.flask_client import OAuth
 import os
 import requests
 
-app = Flask(__name__, 
-            static_folder='frontend/static', 
-            template_folder='frontend/templates')
+app = Flask(__name__)
 application = app
 app.secret_key = os.environ.get("SECRET_KEY", "dev_secret_key")
 API_GATEWAY_URL = os.environ.get(
